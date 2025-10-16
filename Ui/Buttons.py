6f3078005ba63726bb2462dc4157475d87d678d4
KAKAkomboo <0angelangeli0@gmail.com>
2025-10-16 15:27:30 +0300

    change Buttons.py, Menu.py

--- Ui/Buttons.py
+++ Ui/Buttons.py
@@ -1,6 +1,7 @@
 import pygame
 from Settings import *
 
+
 class Button:
     def __init__(self, x, y, width, height, text):
         self.rect = pygame.Rect(x, y, width, height)
@@ -8,16 +9,28 @@ class Button:
         self.color = White_color
         self.hover_color = Primary_color
         self.text_color = Black_color
+        self.border_color = Black_color
         self.font = Primary_font
         self.hovered = False
+        self.border_radius = 15
 
     def draw(self, screen):
-        color = self.hover_color if self.hovered else self.color
-        pygame.draw.rect(screen, color, self.rect)
-        pygame.draw.rect(screen, color, self.rect, 2)
+        fill_color = self.hover_color if self.hovered else self.color
+
+        pygame.draw.rect(screen, fill_color, self.rect, border_radius=self.border_radius)
+
+        pygame.draw.rect(screen, self.border_color, self.rect, 2, border_radius=self.border_radius)
 
         text_surface = self.font.render(self.text, True, self.text_color)
+        shadow_color = (50, 50, 50)
+        shadow_surface = self.font.render(self.text, True, shadow_color)
+
         text_rect = text_surface.get_rect(center=self.rect.center)
+        shadow_rect = shadow_surface.get_rect(
+            center=(text_rect.centerx + 2, text_rect.centery + 2))
+
+        screen.blit(shadow_surface, shadow_rect)
+
         screen.blit(text_surface, text_rect)
 
     def handle_ev(self, event):
@@ -30,3 +43,4 @@ class Button:
 
     def is_clicked(self, pos):
         return self.rect.collidepoint(pos)
+
--- Ui/Menu.py
+++ Ui/Menu.py
@@ -1,8 +1,7 @@
-# GAME MENU
-
 import pygame
 from Settings import *
 from Ui.Buttons import *
+
 
 class Menu:
     def __init__(self, screen):
@@ -13,23 +12,27 @@ class Menu:
 
         self.button_width = 200
         self.button_height = 60
-        self.button_s = 10
-        self.start_y = (screen_height - (len(self.options) * self.button_height) + (len(self.options) - 1) * self.button_s) // 2
+        self.button_spacing = 10
+        self.start_y = (screen_height - (len(self.options) * self.button_height) + (
+                    len(self.options) - 1) * self.button_spacing) // 2
         self.start_x = (screen_width - self.button_width) // 2
 
         self.buttons = []
         for i, option in enumerate(self.options):
-            y = self.start_y + i * (self.button_height + self.button_s)
+            y = self.start_y + i * (self.button_height + self.button_spacing)
             button = Button(self.start_x, y, self.button_width, self.button_height, option)
             self.buttons.append(button)
 
+
         self.title = Primary_font.render("Bebebbe", True, White_color)
-        self.title_rect = self.title.get_rect(center=(screen_width//2, 100))
+        self.title_rect = self.title.get_rect(center=(screen_width // 2, 100))
 
     def draw(self):
         self.screen.fill(Black_color)
 
+
         self.screen.blit(self.title, self.title_rect)
+
 
         for i, button in enumerate(self.buttons):
             if i == self.selected:
@@ -40,7 +43,8 @@ class Menu:
                 button.color = Primary_color
                 button.hover_color = Secondary_color
                 button.hovered = False
-                button.draw(self.screen)
+
+            button.draw(self.screen)
 
     def handle_ev(self, events):
         for event in events:
@@ -50,9 +54,10 @@ class Menu:
                 elif event.key == pygame.K_DOWN:
                     self.selected = (self.selected + 1) % len(self.options)
                 elif event.key == pygame.K_RETURN:
-                    return self._get_action(self)
+                    return self._get_action(self.selected)
                 elif event.key == pygame.K_ESCAPE:
                     return "quit"
+
 
             for button in self.buttons:
                 if button.handle_ev(event):
